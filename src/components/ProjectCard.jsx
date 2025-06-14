ed0cc4d6e08ae1dfebaf44cb77218fc4eea51f8b
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-14 12:20:21 +0100

    Improve responsiveness in ProjectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,19 @@
 const ProjectCard = ({ title, description, github, demo, image }) => {
   return (
-    <div className="bg-black-100 rounded-lg border border-black-200 overflow-hidden shadow-lg p-6 reveal flex flex-col justify-between md:w-[450px] w-[350px] h-[500px]">
+    <div className="bg-black-100 rounded-lg border border-black-200 
+                    overflow-hidden p-6 reveal flex flex-col 
+                    md:w-[450px] w-[350px] h-[500px]">
       {image && (
-        <img
-          src={image}
-          alt={title}
-          className="w-full h-full object-cover bg-black-200 rounded-md mb-4"
-        />
+        <div className="md:h-[300px] md:w-[400px] h-[280px] w-auto mb-5">
+          <img
+            src={image}
+            alt={title}
+            className="w-auto md:h-[300px] md:w-[400px] h-[280px] object-cover bg-black-200 rounded-md mb-0"
+          />
+        </div>
       )}
       <h3 className="text-lg font-semibold text-white-50">{title}</h3>
-      <p className="text-sm text-white-50 mt-2 flex-grow">{description}</p>
+      <p className="text-sm text-blue-50 mt-2 flex-grow">{description}</p>
       <div className="flex gap-4 mt-4">
         <a
           href={github}
