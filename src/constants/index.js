f70a780f930b2d91bd81ceae8a41ac46585aecc3
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-14 09:20:02 +0100

    Update public image references to use correct paths

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,10 +8,10 @@ const links = [
 
 // Hero Section
 const words = [
-  { text: "Ideas", img: "public/images/ideas.svg" },
-  { text: "Concepts", img: "public/images/concepts.svg" },
-  { text: "Designs", img: "public/images/designs.svg" },
-  { text: "Code", img: "public/images/code.svg" },
+  { text: "Ideas", img: "/images/ideas.svg" },
+  { text: "Concepts", img: "/images/concepts.svg" },
+  { text: "Designs", img: "/images/designs.svg" },
+  { text: "Code", img: "/images/code.svg" },
 ];
 
 // AnimatedCounter
