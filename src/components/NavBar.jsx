9578c3afb7ba8b1f6e4726c8b353cd2b6c51a635
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-16 23:40:57 +0100

    Implement responsive Navbar with Hamburger menu for mobile and full navigation for desktop

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,53 @@
-import NavLinks from "./NavLinks";
+import { useState } from "react";
+import { Spin as Hamburger } from "hamburger-react";
 import Logo from "../assets/dasdev-logo.png";
+import NavLinks from "./NavLinks";
 
-const NavBar = () => {
+const NavBar = ({ showAnnouncement }) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <header className="navbar bg-black">
-      <div className="inner px-4 py-4 xl:rounded-lg rounded-lg bg-black-100 border border-black-200">
+    <header
+      className={`navbar fixed w-full left-1/2 -translate-x-1/2 md:py-4 py-2.5
+        transition-all duration-300 ease-in-out z-[100] bg-black-100 ${
+          showAnnouncement ? "top-[24px]" : "top-0"
+        }`}
+    >
+      <div className="inner flex items-center justify-between">
+        {/* Logo */}
         <a href="#hero">
           <img
             src={Logo}
             alt="Logo"
-            className="logo px-2  w-36 xl:w-40 h-auto"
+            className="w-36 xl:w-40 h-auto transition-transform duration-300 hover:scale-105"
           />
         </a>
-        <nav className="desktop">
+
+        {/* Desktop Nav */}
+        <nav className="desktop hidden lg:flex">
           <NavLinks />
         </nav>
-        <a href="#contact" className="contact-btn group">
+
+        {/* Contact Button - only desktop */}
+        <a href="#contact" className="hidden lg:block contact-btn group">
           <div className="inner">
             <span>Contact Me</span>
           </div>
         </a>
+
+        {/* Hamburger - only mobile */}
+        <div className="lg:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            size={42}
+            color="#d9ecff"
+            rounded
+            label="Toggle menu"
+          />
+        </div>
+
+        {/* Mobile Menu */}
       </div>
     </header>
   );
