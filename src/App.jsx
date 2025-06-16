6e189d541e44b631aa05756d58957f3d23d58825
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-16 23:18:26 +0100

    Import layout component into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
 import Projects from "./sections/Projects";
 import Footer from "./sections/Footer";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Hero />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </Layout>
   );
 }
 
