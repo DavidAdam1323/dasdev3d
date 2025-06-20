ae4ff96aa9c0ef3233fbfd80dceb4aa1b8f8e596
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-20 10:36:18 +0100

    Externalize mobile nav links and integrate icon rendering

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import { Spin as Hamburger } from "hamburger-react";
+import { mobileNavLinks } from "../constants/index";
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-
-  const mobileNavLinks = [
-    { name: "Home", link: "#hero" },
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Contact", link: "#contact" },
-  ];
 
   return (
     <div className="relative lg:hidden">
@@ -28,7 +22,7 @@ const MobileNav = () => {
 
       {/* Drop-Down Menu */}
       <div
-        className={`absolute w-97.5 bg-black-100 shadow-xl right-0 mt-3 rounded-md 
+        className={`absolute w-[15ch] sm:w-[20ch] px-1 sm:px-2 bg-black-200 shadow-xl right-0 mt-8 rounded-md 
         z-50 transition-all duration-300 before:content-[''] before:absolute before:w-4 
         before:h-4 before:bg-black-200 before:rotate-45 before:right-4 before:-top-2
         ${
@@ -37,15 +31,18 @@ const MobileNav = () => {
             : "opacity-0 invisible pointer-events-none"
         }`}
       >
-        <ul className="flex flex-col divide-y divide-black-200">
-          {mobileNavLinks.map(({ name, link }) => (
+        <ul className="flex flex-col divide-y divide-blue-50">
+          {mobileNavLinks.map(({ icon: Icon, name, link }) => (
             <li key={name}>
               <a
                 href={link}
-                className="block px-5 py-3 text-xl text-blue-50"
+                className="block px-5 py-3 text-xl sm:text-2xl sm:py-6 text-white-50"
                 onClick={() => setIsOpen(false)}
               >
-                {name}
+                <div className="flex items-center">
+                  <Icon className="text-blue-50 mr-2" />
+                  {name}
+                </div>
               </a>
             </li>
           ))}
