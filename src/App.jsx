bc4266c841b18d94edf56a8ffc7983ebfaf4e865
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-21 02:03:48 +0100

    Reorder imports in App.jsx for improved logical grouping

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Layout from "./layout/Layout";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
 import Projects from "./sections/Projects";
 import Footer from "./sections/Footer";
-import Layout from "./layout/Layout";
 
 function App() {
   return (
