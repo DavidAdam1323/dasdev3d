64a639b3c04d953d309465dbfdd06ef5b6056830
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-21 02:09:31 +0100

    Created a reusable NavItem component that dynamically renders different styles based on the 'variant' prop, supporting both mobile and desktop navigation layouts

---
+++ src/components/NavItem.jsx
@@ -0,0 +1,30 @@
+const NavItem = ({ id, name, icon: Icon, onClick, variant = "desktop" }) => {
+  const isMobile = variant === "mobile";
+  return (
+    <li
+      key={name}
+      className={`relative group ${isMobile ? "text-white" : "text-white-50"}`}
+    >
+      <a
+        href={id}
+        title={name}
+        onClick={onClick}
+        className={`flex items-center ${
+          isMobile
+            ? "px-5 sm:pl-7 py-3 text-xl sm:text-2xl sm:py-6"
+            : "py-2 px-3 md:p-0"
+        } hover:text-white`}
+      >
+        {Icon && (
+          <Icon
+            className="mr-2 text-blue-50"
+          />
+        )}
+        <span>{name}</span>
+        <span className="absolute left-0 -bottom-0.5 h-0.5 bg-white w-0 transition-all duration-300 group-hover:w-full"></span>
+      </a>
+    </li>
+  );
+};
+
+export default NavItem;
