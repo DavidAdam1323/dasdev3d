ab7f2ea6f9e9826441f611c407532123329499fc
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-21 02:19:27 +0100

    Refactor MobileNav to use shared NavItem component and navLinks data

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Spin as Hamburger } from "hamburger-react";
-import { mobileNavLinks } from "../constants/index";
+import { navLinks } from "../constants/index";
+import NavItem from "./NavItem";
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,7 +9,7 @@ const MobileNav = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <div className="relative lg:hidden">
+    <nav className="relative lg:hidden">
       {/* Hamburger Menu Icon */}
       <div onClick={toggleMenu}>
         <Hamburger
@@ -21,10 +22,10 @@ const MobileNav = () => {
       </div>
 
       {/* Drop-Down Menu */}
-      <div
-        className={`absolute w-[15ch] sm:w-[20ch] px-1 sm:px-2 bg-black-200 shadow-xl right-0 mt-8 rounded-md 
+      <menu
+        className={`absolute w-[15ch] sm:w-[20ch] px-1 sm:px-2 bg-gray-900 shadow-xl right-0 mt-6 sm:mt-8 rounded-md 
         z-50 transition-all duration-300 before:content-[''] before:absolute before:w-4 
-        before:h-4 before:bg-black-200 before:rotate-45 before:right-4 before:-top-2
+        before:h-4 before:bg-gray-900 before:rotate-45 before:right-4 before:-top-2
         ${
           isOpen
             ? "opacity-100 visible"
@@ -32,23 +33,19 @@ const MobileNav = () => {
         }`}
       >
         <ul className="flex flex-col divide-y divide-blue-50">
-          {mobileNavLinks.map(({ icon: Icon, name, link }) => (
-            <li key={name}>
-              <a
-                href={link}
-                className="block px-5 py-3 text-xl sm:text-2xl sm:py-6 text-white-50"
-                onClick={() => setIsOpen(false)}
-              >
-                <div className="flex items-center">
-                  <Icon className="text-blue-50 mr-2" />
-                  {name}
-                </div>
-              </a>
-            </li>
+          {navLinks.map(({ id, name, icon }) => (
+            <NavItem
+              key={name}
+              id={id}
+              name={name}
+              icon={icon}
+              variant="mobile"
+              onClick={() => setIsOpen(false)}
+            />
           ))}
         </ul>
-      </div>
-    </div>
+      </menu>
+    </nav>
   );
 };
 
