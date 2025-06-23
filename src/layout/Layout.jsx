4663dce11313248cd582aef885023c6eb97ba290
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-23 14:51:21 +0100

    Simplify Layout by removing scroll-based AnnouncementBar visibility

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react";
 import AnnouncementBar from "../components/AnnouncementBar";
 import NavBar from "../components/NavBar";
 
 const Layout = ({ children }) => {
-  const [showAnnouncement, setShowAnnouncement] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY <= 10) {
-        setShowAnnouncement(true);
-      } else {
-        setShowAnnouncement(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <>
-      <AnnouncementBar show={showAnnouncement} />
-      <NavBar showAnnouncement={showAnnouncement} />
+    <div className="relative">
+      <AnnouncementBar />
+      <NavBar />
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
