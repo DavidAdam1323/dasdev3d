6f0f6a7f92a97b759e1521e3a4019b325b283e4d
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-23 13:43:22 +0100

    Exclude Contact link from desktop navigation

--- src/components/DeskNav.jsx
+++ src/components/DeskNav.jsx
@@ -2,10 +2,12 @@ import { navLinks } from "../constants/index";
 import NavItem from "./NavItem";
 
 const DeskNav = () => {
+  const desktopNavLinks = navLinks.slice(0, -1)
+
   return (
     <div className="items-center justify-between hidden w-full md:w-auto lg:block">
       <ul className="flex font-medium p-4 md:p-0 mt-4 md:mt-0 md:space-x-8">
-        {navLinks.map(({ id, name, icon }) => (
+        {desktopNavLinks.map(({ id, name, icon }) => (
           <NavItem
             key={name}
             id={id}
