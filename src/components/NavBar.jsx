c09e452d332e8f1f83e4626b7b5dbd156ba41553
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-23 15:35:37 +0100

    Update NavBar layout and styling with sticky header, logo animation, and CTA button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,31 @@
 import DeskNav from "./DeskNav";
 import MobileNav from "./MobileNav";
+import CTAButton from "../components/CTAButton";
+import logo from "../assets/dasdev-logo.png";
+import { motion } from "framer-motion";
 
-const NavBar = ({ showAnnouncement }) => {
+const NavBar = () => {
   return (
-    <header
-      className={`fixed z-[100] w-full left-1/2 -translate-x-1/2 px-5 md:px-10 xl:px-20 
-        bg-gray-900 transition-all duration-300 ease-in-out ${
-          showAnnouncement ? "top-[18px] sm:top-[24px]" : "top-0"
-        }`}
-    >
-      <nav className="flex items-center justify-between py-1 md:py-2 lg:py-4">
-        <a href="#home">
-          <img
-            src="src/assets/dasdev-logo.png"
-            alt="Logo"
-            className="h-7 transition-transform duration-300 hover:scale-105"
+    <header className="sticky top-0 z-50 w-full bg-gray-900">
+      <nav className="flex items-center justify-between px-5 md:px-10 xl:px-20 py-3">
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        >
+          <motion.img
+            src={logo}
+            alt="logo"
+            whileTap={{ scale: 0.96 }}
+            whileHover={{ scale: 1.05 }}
+            className="h-8"
           />
         </a>
         <DeskNav />
         <MobileNav />
-        <div className="hidden lg:block space-x-3 md:space-x-0">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
-                focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-          >
-            Get started
-          </button>
-        </div>
+        <CTAButton />
       </nav>
     </header>
   );
