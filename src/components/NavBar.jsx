acb78ada8a64e8e80fa8982dfdea31e3f5105197
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-24 11:57:06 +0100

    Style(navbar): adjust logo size and responsive padding

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 const NavBar = () => {
   return (
     <header className="sticky top-0 z-50 w-full bg-gray-900">
-      <nav className="flex items-center justify-between px-5 md:px-10 xl:px-20 py-3">
+      <nav className="flex items-center justify-between px-5 md:px-10 xl:px-20 py-1.5 sm:py-2 md:py-2.5 lg:py-3.5">
         <a
           href="/"
           onClick={(e) => {
@@ -20,7 +20,7 @@ const NavBar = () => {
             alt="logo"
             whileTap={{ scale: 0.96 }}
             whileHover={{ scale: 1.05 }}
-            className="h-8"
+            className="w-40 h-auto"
           />
         </a>
         <DeskNav />
