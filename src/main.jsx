9f719163ee18831930f5fea1b039ebddc8491ce3
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-27 13:11:34 +0100

    Wrap app with Toaster notification and apply initial styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-    <Toaster position="top-center" reverseOrder={false} />
-  </React.StrictMode>,
-)
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      toastOptions={{
+        style: {
+          background: "var(--blue-50)",
+          color: "black",
+          borderRadius: "8px",
+        },
+      }}
+    />
+  </StrictMode>
+);
