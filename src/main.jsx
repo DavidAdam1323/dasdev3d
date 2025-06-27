98d5e7a7a2fa6d73c0beeeca7d58ca43cb2fc0e1
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-27 10:58:48 +0100

    Wrap App in Toaster for notifications

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Toaster position="top-center" reverseOrder={false} />
+  </React.StrictMode>,
 )
