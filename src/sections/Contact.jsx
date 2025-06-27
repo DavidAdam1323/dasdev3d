9e07a8e4c2518bb77a5c0389b07b98237e2c77a3
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-27 17:07:02 +0100

    Refactor, simplify Contact page with ContactForm and Section components

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,20 +1,11 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import useScrollReveal from "../hooks/useScrollReveal";
-import emailjs from "@emailjs/browser";
-
-import TitleHeader from "../components/TitleHeader";
-import ContactExperience from "../components/models/contactModel/ContactExperience";
+import Section from "../components/Section";
+import ContactForm from "../components/ContactForm";
+import { FaBriefcase } from "react-icons/fa";
 
 const Contact = () => {
-  const formRef = useRef(null);
   const contactRef = useRef(null);
-
-  const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
 
   const animate = useScrollReveal();
 
@@ -26,121 +17,38 @@ const Contact = () => {
     });
   }, [animate]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true); // Show loading state
-
-    try {
-      await emailjs.sendForm(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        formRef.current,
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      );
-
-      // Reset form and stop loading
-      setForm({ name: "", email: "", message: "" });
-    } catch (error) {
-      console.error("EmailJS Error:", error); // Optional: show toast
-    } finally {
-      setLoading(false); // Always stop loading, even on error
-    }
-  };
-
   return (
-    <section
-      id="contact"
-      ref={contactRef}
-      className="relative overflow-hidden pt-45"
-    >
-      <div className="mx-auto px-5 w-full h-full md:px-20">
-        <TitleHeader
-          eyebrowText="Get In Touch"
-          titleText="Let's Create Digital Experiences"
-        />
-
-        <div className="grid-12-cols xl:mt-16 mt-9 reveal">
-          {/* CONTACT FORM -LEFT SIDE */}
-          <div className="xl:col-span-5">
-            <div className="flex-center card-border rounded-lg w-full h-[550px] md:py-5 py-5 px-5">
-              <form
-                ref={formRef}
-                onSubmit={handleSubmit}
-                className="w-full flex flex-col gap-7"
-              >
-                <div>
-                  <label htmlFor="name">Name:</label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={form.name}
-                    onChange={handleChange}
-                    placeholder="Your Full Name..."
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={form.email}
-                    onChange={handleChange}
-                    placeholder="Your Email Address..."
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="message">Message:</label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    value={form.message}
-                    onChange={handleChange}
-                    placeholder="Enter your message here..."
-                    rows="5"
-                    required
-                  />
-                </div>
-
-                <button type="submit" disabled={loading} className="reveal">
-                  <div className="cta-button group">
-                    <div className="bg-circle" />
-                    <p className="text">
-                      {loading ? "Sending..." : "Send Message"}
-                    </p>
-                    <div className="arrow-wrapper">
-                      <img src="/images/arrow-down.svg" alt="arrow" />
-                    </div>
-                  </div>
-                </button>
-              </form>
-            </div>
-          </div>
-
-          {/* 3D EXPERIENCE - RIGHT SIDE */}
-          <div className="xl:col-span-7 min-h-70 md:h-[590px] pb-10">
-            <div
-              className="w-full max-w-[390px] sm:max-w-[500px] xl:max-w-full 
-                h-[280px] sm:h-[400px] xl:h-full border border-[#c78d56]
-                bg-[#cd7c2e] rounded-lg overflow-hidden 
-                grid place-items-center hover:cursor-grab"
-            >
-              <ContactExperience />
-            </div>
+    <Section id={"contact"} ref={contactRef}>
+      <div className="w-full max-w-[400px] flex flex-col justify-center items-center gap-5">
+        <div>
+          <div className="space-y-4 text-[var(--white-50)] max-w-3xl">
+          {/* Header / Intro */}
+            <h2 className="text-2xl text-white sm:text-3xl font-bold leading-snug">
+              Let's Connect and Collaborate
+            </h2>
+            <p className="text-[var(--blue-50)] text-sm sm:text-base leading-relaxed">
+              Whether you,re starting a new project, need help solving a complex
+              problem, or simply want to explore potential collaboration — I'm
+              here to help.
+            </p>
+            <p className="text-[var(--blue-50)] text-sm sm:text-base leading-relaxed">
+              Fill out the form below with as much detail as you'd like. I
+              typically respond within 1-2 business days.
+            </p>
           </div>
         </div>
+
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="w-full flex justify-end items-center pr-4">
+            <p className="pr-1 text-white text-xs sm:text-base leading-relaxed italic">
+              * Professional inquiries only, please.
+            </p>
+            <FaBriefcase className="text-xs text-[var(--blue-50)]" />
+          </div>
+          <ContactForm />
+        </div>
       </div>
-    </section>
+    </Section>
   );
 };
 
