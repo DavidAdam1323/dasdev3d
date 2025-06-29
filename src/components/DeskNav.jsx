fc14e11156348a72aa97e58cc9c89d819a859d3a
DavidAdam1323 <davidadamgtr@gmail.com>
2025-06-29 01:06:16 +0100

    Update font weight for desktop navigation

--- src/components/DeskNav.jsx
+++ src/components/DeskNav.jsx
@@ -6,7 +6,7 @@ const DeskNav = () => {
 
   return (
     <div className="items-center justify-between hidden w-full md:w-auto lg:block">
-      <ul className="flex font-medium p-4 md:p-0 mt-4 md:mt-0 md:space-x-8">
+      <ul className="flex p-4 md:p-0 mt-4 md:mt-0 md:space-x-8">
         {desktopNavLinks.map(({ id, name, icon }) => (
           <NavItem
             key={name}
