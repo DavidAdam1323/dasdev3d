e820d4803a4dc83047bb0c688fcd2f8524d63466
DavidAdam1323 <davidadamgtr@gmail.com>
2025-07-27 13:12:22 +0100

    Enhance ProjectCard UI with icons and updated styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,11 @@
-import { FiExternalLink, FiGithub } from "react-icons/fi";
+import { FiExternalLink, FiGithub } from "react-icons/fi";  
 
 const ProjectCard = ({ title, description, github, demo, image }) => {
   return (
-    <div className="bg-[var(--black-100)] rounded-lg border border-[var(--black-50)] overflow-hidden 
-                    p-5 reveal flex flex-col sd:max-w-[400px] md:w-[450px] w-[350px] h-[550px]">
+    <div
+      className="bg-[var(--black-100)] rounded-lg border border-[var(--black-50)] overflow-hidden 
+                    p-5 reveal flex flex-col sd:max-w-[400px] md:w-[450px] w-[350px] h-[550px]"
+    >
       {image && (
         <div className="relative md:h-[300px] md:w-[400px] h-[280px] w-auto mb-5">
           <img
@@ -14,7 +16,9 @@ const ProjectCard = ({ title, description, github, demo, image }) => {
         </div>
       )}
       <h3 className="text-lg font-semibold text-[var(--white-50)]">{title}</h3>
-      <p className="text-sm text-[var(--blue-50)] mt-2 flex-grow">{description}</p>
+      <p className="text-sm text-[var(--blue-50)] mt-2 flex-grow">
+        {description}
+      </p>
       <div className="flex flex-col justify-center gap-4 mt-4 w-full">
         <a
           href={github}
